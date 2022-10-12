72d27719102f16908a292cac3170f7fdb2915bb9
Kristian Apostolov <kristian@Kristians-MacBook-Pro.local>
2022-10-12 13:56:27 +0200

    Update requirements

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker build -t biomage/programmatic-interface:0.0.1 .
+docker build -t biomage/programmatic-interface:0.0.3 .
 
 docker login
-docker push biomage/programmatic-interface:0.0.1
+docker push biomage/programmatic-interface:0.0.3
