db7efc51bafb27b2c1c731e3bd1cbd535b0e6e91
stefanbabukov <stefanbabukov98@gmail.com>
2023-02-01 13:01:39 +0000

    oops

--- test.py
+++
@@ -1,7 +0,0 @@
-import biomage_programmatic_interface as bpi
-
-c = bpi.Connection('stefan@biomage.net', 'Fu2.QAG}', 'local')
-exp = c.create_experiment('molq te raboti')
-exp.upload_samples('../../../sample1')
-# exp_clone = exp.clone('0251e46d-02eb-4eff-a64f-a5ce9ed44d40')
-exp.run()
